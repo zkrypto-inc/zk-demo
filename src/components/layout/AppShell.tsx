95b9b56a793769099b457d51288b74e89155c10a
sjounng <jwsong5160@gmail.com>
2026-04-30 10:59:06 +0900

    hotfix: 탑바 삭제

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -1,7 +1,6 @@
 import type { ReactNode } from "react";
 import type { DemoRoute } from "@/router";
 import { scenarioGroupLookup } from "@/scenarios";
-import { TopBar } from "./TopBar";
 import { SideNav } from "./SideNav";
 
 type Props = {
@@ -25,7 +24,6 @@ export function AppShell({ children, route }: Props) {
       <div className="flex min-h-screen">
         <SideNav currentActorId={currentActorId} currentScenarioId={currentScenarioId} />
         <div className="min-w-0 flex-1">
-          <TopBar />
           <main className="px-5 pb-14 pt-6 xl:px-8 2xl:px-10">
             <div className="mx-auto max-w-[1760px]">{children}</div>
           </main>
